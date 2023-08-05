139355f891abc8be464772d4525de1691698dd2d
Salah Zanati <salahzanati247@gmail.com>
2023-08-05 12:13:51 +0300

    fix some issues

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -3,16 +3,6 @@ import { countriesContext } from "../App";
 
 const Filters = () => {
   let contextObj = useContext(countriesContext);
-  const getCountriesByRegion = (region) => {
-    if (region === "all") contextObj.setData(contextObj.countriesData);
-    else {
-      let array = [];
-      contextObj.countriesData.forEach((country) => {
-        country.region === region && array.push(country);
-      });
-      contextObj.setData(array);
-    }
-  };
   return (
     <>
       <select
@@ -20,12 +10,10 @@ const Filters = () => {
           contextObj.setRegion(e.target.value);
         }}
         style={{
-          color: `hsl(${
-            contextObj.mood.current[`${contextObj.moodState}`].text
-          })`,
-          backgroundColor: `hsl(${
-            contextObj.mood.current[`${contextObj.moodState}`].elements
-          })`,
+          color: `hsl(${contextObj.mood.current[`${contextObj.moodState}`].text
+            })`,
+          backgroundColor: `hsl(${contextObj.mood.current[`${contextObj.moodState}`].elements
+            })`,
         }}
       >
         <option value="all">All regions</option>
